9f3b802bfda5c89eeb279d7842ed6536bf51aba8
Gara Luzardo <garaluzardo@gmail.com>
2025-03-08 15:46:43 +0000

    AccessFormPage  warning corregido

--- src/pages/AccessFormPage/AccessFormPage.jsx
+++ src/pages/AccessFormPage/AccessFormPage.jsx
@@ -36,8 +36,6 @@ const Signup = ({ toggleForm }) => {
   const [name, setName] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
   
-  const navigate = useNavigate();
-
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
   const handleName = (e) => setName(e.target.value);
